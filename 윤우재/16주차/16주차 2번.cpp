12ef47b3c9d701a7fe5b3729ef8417bdb2a73225
clairhardt98 <woojae0107@gmail.com>
2023-11-26 14:27:28 +0900

    [16주차 1번] Jump Game / 1시간 / https://leetcode.com/problems/jump-game/description/

---
+++ 윤우재/16주차/16주차 2번.cpp
@@ -0,0 +1,13 @@
+class Solution {
+
+public:
+	bool canJump(vector<int>& nums) {
+		int temp = 0;
+        for(int i = 0; i<nums.size();i++)
+        {
+            if(i>temp) return false;
+            temp = max(temp, i + nums[i]);
+        }
+        return true;
+	}
+};
